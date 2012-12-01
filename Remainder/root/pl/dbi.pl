4dea886fc9c2038f1fededca5fdbbe0072a8345c
th4 <th4@th4-K53E.(none)>
2012-12-02 07:28:22 +0900

    dbi.pl ok

--- Remainder/root/pl/dbi.pl
+++ Remainder/root/pl/dbi.pl
@@ -28,21 +28,22 @@ my $sth = $db->prepare("select id,userid,memo,tag,fromtime,totime,days from Rema
 #my $hour = 17;#given
 #my $days = "Sun,Mon,Wed,Sat";#given
 
-
-
 if(!$sth->execute){
     print "SQL失敗\n";
     exit;
 }
 
 while (my @rec = $sth->fetchrow_array) {
-    print "$rec[0]\n";
-    print "$rec[1]\n";
-    print "$rec[2]\n";
-    print "$rec[3]\n";
-    print "$rec[4]\n";
-    print "$rec[5]\n";
-    print "$rec[6]\n";
+    print "$rec[0]\n";    #id
+    print "$rec[1]\n";    #userid
+    print "$rec[2]\n";    #memo
+    print "$rec[3]\n";    #tag
+    print "$rec[4]\n";    #fromtime
+    my @fromminhour= split(/\s|:/,$rec[4]);
+    #hour:$minhour[1],min:$minhour[2]
+    print "$rec[5]\n";    #totime
+    print "$rec[6]\n";    #days
+    my @days= split(/,/,$rec[6]);
     print "\n";
 }
 
